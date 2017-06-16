91d98e211dc6722b969a4a13baa27302c4500490
习成_taurusxi <ch.xi@onedayapp.cn>
2017-06-16 15:55:00 +0800

    Initial Commit

---
+++ SwiftSingleClick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSingleClick
+//
+//  Created by xicheng on 2017/6/16.
+//  Copyright © 2017年 xicheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
